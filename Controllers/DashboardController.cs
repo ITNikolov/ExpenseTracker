14d268d1ddd4d770fd29e12e049eafe637ca59ed
ITNikolov <ivaylonikolov.n@gmail.com>
2024-07-09 21:15:30 +0300

    started dashboard and sidebar

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,48 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.JSInterop.Implementation;
+
+namespace ExpenseTracker.Controllers
+{
+	public class DashboardController : Controller
+	{
+
+		private readonly ApplicationDbContext _context;
+
+		public DashboardController(ApplicationDbContext context) 
+		{ 
+			_context = context;
+		}
+
+		public async Task<ActionResult> Index()
+		{
+			//Last 7 days
+			DateTime StartDate = DateTime.Today.AddDays(-6);
+			DateTime EndDate = DateTime.Today;
+
+			List<Transaction> SelectedTransaction = await _context.Transactions
+				.Include(x=>x.Category)
+				.Where(y => y.Date>= StartDate && y.Date<=EndDate)
+				.ToListAsync();
+
+			//Total Income
+			int TotalIncome = SelectedTransaction
+				.Where(i => i.Category.Type == "Income")
+				.Sum(j => j.Amount);
+			ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+			//Total Expense
+			int TotalExpense = SelectedTransaction
+				.Where(i => i.Category.Type == "Expense")
+				.Sum(j => j.Amount);
+			ViewBag.TotalExpense = TotalExpense.ToString("C0");
+
+			//Balance
+			int Balance = TotalIncome - TotalExpense;
+			ViewBag.Balance = Balance.ToString("C0");
+
+			return View();
+		}
+	}
+}
