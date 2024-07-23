c95263be47eb0cff79139643990d31ed0a5d37f9
ITNikolov <ivaylonikolov.n@gmail.com>
2024-07-23 08:17:01 +0300

    fixed nav bar, added profile pic , fixed stuff onto charts and added recent transactions

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -11,8 +11,8 @@ namespace ExpenseTracker.Controllers
 
 		private readonly ApplicationDbContext _context;
 
-		public DashboardController(ApplicationDbContext context) 
-		{ 
+		public DashboardController(ApplicationDbContext context)
+		{
 			_context = context;
 		}
 
@@ -23,8 +23,8 @@ namespace ExpenseTracker.Controllers
 			DateTime EndDate = DateTime.Today;
 
 			List<Transaction> SelectedTransaction = await _context.Transactions
-				.Include(x=>x.Category)
-				.Where(y => y.Date>= StartDate && y.Date<=EndDate)
+				.Include(x => x.Category)
+				.Where(y => y.Date >= StartDate && y.Date <= EndDate)
 				.ToListAsync();
 
 			//Total Income
@@ -43,7 +43,7 @@ namespace ExpenseTracker.Controllers
 			int Balance = TotalIncome - TotalExpense;
 			CultureInfo culture = CultureInfo.CreateSpecificCulture("en-USD");
 			culture.NumberFormat.CurrencyNegativePattern = 1;
-			ViewBag.Balance = String.Format(culture,"{0:C0}",Balance);
+			ViewBag.Balance = String.Format(culture, "{0:C0}", Balance);
 
 			//Dougahnut Chart - Expense by Category
 			ViewBag.DoughnutChartData = SelectedTransaction
@@ -51,14 +51,67 @@ namespace ExpenseTracker.Controllers
 				.GroupBy(j => j.Category.CategoryId)
 				.Select(k => new
 				{
-					categoryTittleWithIcon = k.First().Category.Icon+" "+ k.First().Category.Title,
+					categoryTittleWithIcon = k.First().Category.Icon + " " + k.First().Category.Title,
 					amount = k.Sum(j => j.Amount),
 					formattedAmount = k.Sum(j => j.Amount).ToString("C0"),
 				})
+				.OrderByDescending(l => l.amount)
 				.ToList();
 
+			//Spline Chart Income vs Expense
+			//Income
+			List<SplineChartData> IncomeSummary = SelectedTransaction
+				.Where(i => i.Category.Type == "Income")
+				.GroupBy(j => j.Date)
+				.Select(k => new SplineChartData()
+				{
+					day = k.First().Date.ToString("dd-MMM"),
+					income = k.Sum(l => l.Amount)
+				})
+				.ToList();
+
+			//Expense
+			List<SplineChartData> ExpenseSummary = SelectedTransaction
+				.Where(i => i.Category.Type == "Expense")
+				.GroupBy(j => j.Date)
+				.Select(k => new SplineChartData()
+				{
+					day = k.First().Date.ToString("dd-MMM"),
+					expense = k.Sum(l => l.Amount)
+				})
+				.ToList();
+
+			//Combine Income & Expense
+			string[] Last7Days = Enumerable.Range(0, 7)
+				.Select(i => StartDate.AddDays(i).ToString("dd-MMM"))
+				.ToArray();
+
+			ViewBag.SplineChartData = from day in Last7Days
+									  join income in IncomeSummary on day equals income.day into dayIncomeJoined
+									  from income in dayIncomeJoined.DefaultIfEmpty()
+									  join expense in ExpenseSummary on day equals expense.day into expenseJoined
+									  from expense in expenseJoined.DefaultIfEmpty()
+									  select new
+									  {
+										  day = day,
+										  income = income == null ? 0 : income.income,
+										  expense = expense == null ? 0 : expense.expense,
+									  };
+			//Recent Transactions
+			ViewBag.RecentTransactions = await _context.Transactions
+				.Include(i => i.Category)
+				.OrderByDescending(j =>j.Date)
+				.Take(5)
+				.ToListAsync();
 
 			return View();
 		}
+	}
+
+	public class SplineChartData
+	{
+		public string day;
+		public int income;
+		public int expense;
 	}
 }
